725255f57fa409c5b16150633bef367f6fb3e176
sth122 <132740922+sth122@users.noreply.github.com>
2023-06-20 21:07:12 +0900

    Image_download.py
    csv 파일에 이미지경로 지정

--- 관리자모드/Image_download.py
+++ 관리자모드/Image_download.py
@@ -1,9 +1,9 @@
-# 상의, 하의 이미지 파일 위치경로를 csv에 저장하는 코드
+#상의, 하의 이미지 파일 위치경로를 csv에 저장하는 코드
 import csv
 import os
 
 # 상의 이미지 파일이 있는 디렉토리 경로
-image_dir = r'C:\\파이썬 1조\\상의 목록\\스포츠\\spring_sports_high'
+image_dir = r'C:\\파이썬 1조\\상의 목록\\캐주얼\\spring_casual_high'
 
 image_files = os.listdir(image_dir)
 
@@ -12,14 +12,24 @@ image_files = os.listdir(image_dir)
 # CSV에 이미지 파일 경로 저장 시 csv 파일 형식 변환 필수 #
 # 변환 후 인터페이스 실행 시 다시 파일 형식 복구 필수    #
 ##########################################################
-csv_file = 'C:\\파이썬 1조\\상의 목록\\스포츠\\spring_sports_high.csv'
-if ext in ['.jpg', '.jpeg', '.png', '.gif']:
+csv_file = 'C:\\파이썬 1조\\상의 목록\\csv파일\\캐주얼\\spring_casual_high_read.csv'
+
+
+
+# 각 이미지 파일에 대해 이미지 경로를 추가하여 저장
+for i, file_name in enumerate(image_files):
+    # 파일 확장자 확인 (예: .jpg, .png 등)
+    #os.path.splitext 파일 확장자 확인하는 splitext 함수
+    ext = os.path.splitext(file_name)[-1].lower()
+
+    # 이미지 파일인 경우에만 처리
+    if ext in ['.jpg', '.jpeg', '.png', '.gif']:
         # 이미지 파일의 전체 경로 생성
-        file_path = os.path.join("C:\\\\파이썬 1조\\\\상의 목록\\\\스포츠\\\\spring_sports_high\\\\", str(i+1))
+        file_path = os.path.join("C:\\\\파이썬 1조\\\\상의 목록\\\\캐주얼\\\\spring_casual_high\\\\", str(i+1))
 
         # 이미지 파일 확장자 추가
         file_path += ".jpg"
-
+        
         # 이미지 파일의 이름을 추출
         name = os.path.splitext(file_name)[0]
 
@@ -39,10 +49,8 @@ if ext in ['.jpg', '.jpeg', '.png', '.gif']:
 
 print("상의 이미지 경로가 성공적으로 추가되었습니다.")
 
-
-
 # 하의 이미지 파일이 있는 디렉토리 경로
-image_dir1 = r'C:\\파이썬 1조\\하의 목록\\스포츠\\spring_training_low'
+image_dir1 = r'C:\\파이썬 1조\\하의 목록\\캐주얼\\spring_casual_low'
 
 image_files1 = os.listdir(image_dir1)
 
@@ -51,7 +59,7 @@ image_files1 = os.listdir(image_dir1)
 # CSV에 이미지 파일 경로 저장 시 csv 파일 형식 변환 필수 #
 # 변환 후 인터페이스 실행 시 다시 파일 형식 복구 필수    #
 ##########################################################
-csv_file1 = 'C:\\파이썬 1조\\하의 목록\\csv파일\\스포츠\\spring_training_low_read.csv'
+csv_file1 = 'C:\\파이썬 1조\\하의 목록\\csv파일\\캐주얼\\spring_casual_low_read.csv'
 
 
 
@@ -64,7 +72,7 @@ for i, file_name1 in enumerate(image_files1):
     # 이미지 파일인 경우에만 처리
     if ext1 in ['.jpg', '.jpeg', '.png', '.gif']:
         # 이미지 파일의 전체 경로 생성
-        file_path1 = os.path.join("C:\\\\파이썬 1조\\\\하의 목록\\\\스포츠\\\\spring_training_low\\\\", str(i+1))
+        file_path1 = os.path.join("C:\\\\파이썬 1조\\\\하의 목록\\\\캐주얼\\\\spring_casual_low\\\\", str(i+1))
 
         # 이미지 파일 확장자 추가
         file_path1 += ".jpg"
@@ -87,4 +95,3 @@ for i, file_name1 in enumerate(image_files1):
             writer1.writerows(rows1)
 
 print("하의 이미지 경로가 성공적으로 추가되었습니다.")
-
